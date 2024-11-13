af0f040d1eca35d4b1024f9e9af46ea6d0e6b9cf
paulmay <paul.may@sdsmt.edu>
2024-11-13 08:55:51 -0700

    Added bernoulli_predict.jl

---
+++ src/bernoulli_predict.jl
@@ -0,0 +1,52 @@
+function bernoulli_predict(readdir, Xpred, locpred, timepred, m)
+
+    npred = size(locpred,1)
+
+    params = CSV.read(joinpath(read_dir, "zparams.csv"), DataFrame)
+
+    sw = params.sw
+    rangeS = params.rangeS
+    rangeT = params.rangeT
+
+    beta = Matrix(select(params, r"beta_\d"))
+
+    p = size(beta,2)
+    p == size(Xpred, 2) || error("Read coeffecients of different dims than provided XPred")
+    
+    w = CSV.read(joinpath(read_dir, "wz.csv"), Tables.matrix)
+
+    loctime = CSV.read(joinpath(read_dir, "locTime.csv"), Tables.matrix)
+    loc = loctime[:,1:2]
+    time = loctime[:,[3]]
+
+    nSamps = size(w,1)
+
+    ###
+
+    print("Getting neighbors...\n")
+    nb = getNeighborsP(loc, locpred, m)
+
+    print("Getting initial NNGP mats...\n")
+    B, F, BOrder = getNNGPmatsSTP(nb, loc, time, locpred, timepred, rangeS[1], rangeT[1])
+
+    predSamps = zeros(Int64, nSamps, nProj)
+
+    ###############
+    # Compute mean and variance iteratively
+    ##############
+
+    print("Start predictions with $nSamps samples\n")
+
+    @views for i in ProgressBar(1:nSamps)
+
+        getNNGPmatsSTP!(B, F, BOrder, nb, loc, time, locpred, timepred, rangeS[i], rangeT[i])
+
+        mu = softmax.(Xpred*beta[i,:] + B*w[i,:] + sw[i]*sqrt.(F).*randn(npred))
+
+        predSamps[i,:] = 1*(rand(npred) .< mu)
+
+    end
+
+    return predSamps
+
+end
