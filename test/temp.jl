bd6bcc617ea808b779ffcf2c49a4f0cadfbd41ef
paulmay <paul.may@sdsmt.edu>
2024-12-11 09:02:06 -0700

    Added CV to temp.jl

--- test/temp.jl
+++ test/temp.jl
@@ -11,7 +11,7 @@ using Distributions
 n = 1000
 nUnq = Integer(floor(n / 4))
 
-nPred = 1000
+nPred = 5000
 nPredUnq = Integer(floor(nPred / 4))
 
 Random.seed!(96)
@@ -28,7 +28,7 @@ locFull = vcat(loc, locPred)
 timeFull = vcat(time, timePred)
 #
 
-posparams = (beta = 5, sw1 = 2, rangeS1 = 0.2, rangeT1 = 1, sw2 = 1, rangeT2 = 0.5, tSq = 0.01)
+posparams = (beta = 5, sw1 = 5, rangeS1 = 0.2, rangeT1 = 1, sw2 = 1, rangeT2 = 0.5, tSq = 0.01)
 zparams = (beta = 2, sw = 4, rangeS = 0.3, rangeT = 2)
 
 m = 25
@@ -66,7 +66,7 @@ end
 #
 
 priors_pos = (theta10 = [1, 0.1, 1], alpha10 = [0.5, 0.5, 0.5], theta20 = [1, 1], alpha20 = [0.5, 0.5])
-priors_z = (theta0 = [1, 0.1, 1], alpha0 = [0.5, 0.5, 0.5], beta = [2 20])
+priors_z = (theta0 = [1, 0.1, 1], alpha0 = [0.5, 0.5, 0.5], beta = [2 1])
 
 timeKnots = reshape( collect(0:0.1:1), :, 1)
 
@@ -82,7 +82,7 @@ NNGP_Continuous_MCMC(posdata, m, timeKnots, posmap, priors_pos, thetaVar_pos, ou
 
 thetaVar_pos = getPropVars("./test/dump/yparams.csv", ["sw1", "rangeS1", "rangeT1", "sw2", "rangeT2", "tSq"], nSampsBurn)
 
-nSamps = 1000
+nSamps = 2000
 
 NNGP_Continuous_MCMC(posdata, m, timeKnots, posmap, priors_pos, thetaVar_pos, outDir, nSamps)
 
@@ -108,7 +108,7 @@ NNGP_Bernoulli(zdata, m, zparams, priors_z, outDir, nSampsBurn, adaptStart = 50,
 
 thetaVar_z = getPropVars("./test/dump/zparams.csv", ["sw", "rangeS", "rangeT"], nSampsBurn)
 
-NNGP_Bernoulli(zdata, m, zparams, priors_z, thetaVar_z, outDir, nSamps)
+NNGP_Bernoulli(zdata, m, zparams, priors_z, outDir, nSamps, adaptStart = 50)
 
 pardf_z= CSV.read("./test/dump/zparams.csv", DataFrame)
 
@@ -153,6 +153,10 @@ function getCI(samps::AbstractArray, alpha::Number)
 end
 
 yCI = getCI(ysamps, 0.95)
+
+mean((yCI[:,1] .< yposPred))
+mean((yCI[:,2] .> yposPred))
+
 
 mean((yCI[:,1] .< yposPred) .&& (yCI[:,2] .> yposPred))
 
